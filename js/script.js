19211818b7ef63ff9d1ada0dee95c3b2d24ff01a
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-23 10:42:44 -0400

    add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-// ef8b32bdaca9d5613b4ce1eb35db3d38
+const apiKey = "  ";
+const apiCoutryUrl = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const coutryElement = document.querySelector("#acountry");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+//Funções
+const getWeatherData = async (city) => {
+  const apiWeatherURL =
+};
+
+const showWeatherData = (city) => {
+  console.log(city);
+};
+
+//Eventos
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
